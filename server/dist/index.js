a0259a23098882582fc2a1768ab09a373af996dd
Angel Contreras <c3po.angel@gmail.com>
2025-05-09 10:25:07 -0600

    ajustes backend

--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-console.log("desde index.ts");
-var result = (0, server_1.sumar)();
-console.log("resultado suma", result);
-//# sourceMappingURL=index.js.map
--- server/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sumar = sumar;
-//config de server
-// por medio de handlers
-function sumar() {
-    return 2 + 2 + 6;
-}
-//# sourceMappingURL=server.js.map
